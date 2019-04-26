b7b396808c0d91fd2a2673958c027d944854d7e5
Rais Darmawan Dwi Yulianto <48411442+erdedeye@users.noreply.github.com>
2019-04-27 02:49:55 +0700

    Update raot-event.sql

--- raot-event.sql
+++ raot-event.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 26 Apr 2019 pada 15.01
--- Versi server: 10.1.33-MariaDB
--- Versi PHP: 7.2.6
+-- Generation Time: 26 Apr 2019 pada 21.49
+-- Versi Server: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -34,7 +32,7 @@ CREATE TABLE `event` (
   `foto` varchar(50) NOT NULL,
   `game` varchar(28) NOT NULL,
   `platform` varchar(15) NOT NULL,
-  `tgl` varchar(11) NOT NULL,
+  `tgl` date NOT NULL,
   `deskripsi` text NOT NULL,
   `rules` text NOT NULL,
   `alamat` varchar(32) NOT NULL,
@@ -48,8 +46,9 @@ CREATE TABLE `event` (
 --
 
 INSERT INTO `event` (`idEvent`, `nm_event`, `foto`, `game`, `platform`, `tgl`, `deskripsi`, `rules`, `alamat`, `mode`, `uplink`, `tgl_upload`) VALUES
-(1, 'TOURNAMENT DOTA', '542-dota1.jpg', 'Nama game contoh', 'platform contoh', '1', 'Contoh Deskripsi Event', 'Contoh Rules Event', 'jl. contoh', 1, 2, '0000-00-00'),
-(2, 'MEISTER DIVISION ROOKIES Dota 2 Online Tournament', '9335-dota.jpg', 'Nama game contoh', 'platform contoh', '1', 'Contoh Deskripsi Event', 'Contoh Rules Event', 'jl. contoh', 1, 2, '0000-00-00');
+(1, 'TOURNAMENT DOTA', '542-dota1.jpg', 'Nama game contoh', 'platform contoh', '0000-00-00', 'Contoh Deskripsi Event', 'Contoh Rules Event', 'jl. contoh', 1, 2, '0000-00-00'),
+(2, 'MEISTER DIVISION ROOKIES Dota 2 Online Tournament', '9335-dota.jpg', 'Nama game contoh', 'platform contoh', '0000-00-00', 'Contoh Deskripsi Event', 'Contoh Rules Event', 'jl. contoh', 1, 2, '0000-00-00'),
+(13, 'Mobile Legend HCP(Online Tournament)', '792-ml333.jpg', 'Mobile Legend', 'Mobile', '2019-05-31', 'Desk', 'Rules', 'Online', 1, 2, '2019-04-27');
 
 -- --------------------------------------------------------
 
@@ -72,41 +71,38 @@ CREATE TABLE `panitia` (
 INSERT INTO `panitia` (`nick`, `nama`, `email`, `pass`, `id_panitia`) VALUES
 ('Sandy', 'Sandy', 'sandydwi75@gmail.com', '0ddd63424b5af786fdd47bcc9209bcf3', 12),
 ('raot', 'Raot', 'raot@raot.com', '974fd02a39f94ff7180fff16b3358212', 14),
-('swaxtic', 'Swaxtic Hub', 'swaxtic@raot.id', '21232f297a57a5a743894a0e4a801fc3', 2);
+('swaxtic', 'Swaxtic Hub', 'swaxtic@raot.id', '2e33a9b0b06aa0a01ede70995674ee23', 2);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `event`
+-- Indexes for table `event`
 --
 ALTER TABLE `event`
   ADD PRIMARY KEY (`idEvent`);
 
 --
--- Indeks untuk tabel `panitia`
+-- Indexes for table `panitia`
 --
 ALTER TABLE `panitia`
   ADD PRIMARY KEY (`id_panitia`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `event`
+-- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `idEvent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
+  MODIFY `idEvent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
--- AUTO_INCREMENT untuk tabel `panitia`
+-- AUTO_INCREMENT for table `panitia`
 --
 ALTER TABLE `panitia`
   MODIFY `id_panitia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
